07122d54a63e01d8c1075bb8406a2f81423ecf41
Leila <99736610+codewithleila@users.noreply.github.com>
2022-03-30 08:56:14 -0700

    Create main.js

---
+++ zodiac energy site/main.js
@@ -0,0 +1,67 @@
+document.getElementById('aries').onclick = partyAries
+document.getElementById('taurus').onclick = partyTaurus
+document.getElementById('gemini').onclick = partyGemini
+document.getElementById('cancer').onclick = partyCancer
+document.getElementById('leo').onclick = partyLeo
+document.getElementById('virgo').onclick = partyVirgo
+document.getElementById('libra').onclick = partyLibra
+document.getElementById('scorpio').onclick = partyScorpio
+document.getElementById('sagittarius').onclick = partySagittarius
+document.getElementById('capricorn').onclick = partyCapricorn
+document.getElementById('aquarius').onclick = partyAquarius
+document.getElementById('pisces').onclick = partyPisces
+
+
+function partyAries() {
+  document.querySelector('.rightSide').style.backgroundImage = "url(https://media.giphy.com/media/OhgxAREfQnuGQ/giphy.gif)"
+}
+
+
+function partyTaurus() {
+   document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/YRhJE2tTH29PoYzMaE/giphy.gif')"
+}
+
+
+function partyGemini() {
+  document.querySelector('.rightSide').style.backgroundImage = "url(https://media.giphy.com/media/l092lTtYG7mmRysZso/giphy.gif)"
+}
+
+
+function partyCancer() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/Y0Vq7jHyXoaQCvFN5k/giphy.gif')"
+}
+
+
+function partyLeo() {
+  document.querySelector('.rightSide').style.backgroundImage = "url(https://media.giphy.com/media/cjEq9rAew39nDH6bKh/giphy.gif)"
+}
+
+
+function partyVirgo() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/QYjBjXLiF6I0i0WZH1/giphy.gif')"
+}
+
+function partyLibra() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/l3vQXHFR7V3nTXQWI/giphy.gif')"
+}
+
+function partyScorpio() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/prPvOwr6774spd3rBj/giphy.gif')"
+}
+
+function partySagittarius() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/YSeu0Lj3l8G5jCw1FE/giphy.gif')"
+}
+
+function partyCapricorn() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/oxKHrfcTm4mbu/giphy.gif')"
+}
+
+function partyAquarius() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/WbXv2OztQWdC8/giphy.gif')"
+}
+
+function partyPisces() {
+  document.querySelector('.rightSide').style.backgroundImage = "url('https://media.giphy.com/media/MbLvhV3LQCAQzfAjlT/giphy.gif')"
+}
+
