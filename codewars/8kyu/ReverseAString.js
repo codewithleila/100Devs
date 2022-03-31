8ad73bbd6d76ff4b0aedce176199c5b86471a890
Leila Mohd <99736610+codewithleila@users.noreply.github.com>
2022-03-31 01:46:31 -0700

    Update ReverseAString.js

--- codewars/8kyu/ReverseAString.js
+++ codewars/8kyu/ReverseAString.js
@@ -1,1 +1,21 @@
+/* Complete the solution so that it reverses the string passed into it.
 
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+function solution(str){
+    // return an array of elements of the string
+    var stringArray = str.split("");
+   
+    // reverse the array elements into a new array of elements
+    var reverseArray = stringArray.reverse();
+ 
+    // join the new reversed elements of the array into a string
+    var joinArray = reverseArray.join("");
+    
+    // return reversed string
+    return joinArray;
+}
+ 
