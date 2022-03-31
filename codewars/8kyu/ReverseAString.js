f2a47e8833d8533dfaaabbdc69ef3ec6a31de311
Leila Mohd <99736610+codewithleila@users.noreply.github.com>
2022-03-31 07:15:22 -0700

    Update ReverseAString.js

--- codewars/8kyu/ReverseAString.js
+++ codewars/8kyu/ReverseAString.js
@@ -20,7 +20,14 @@ function solution(str){
 }
  
 
-/* Best Practices Solution */
+/* Best Practices Solution 1*/
 function solution(str){
   return str.split('').reverse().join('');  
 }
+
+/* Best Practices Solution 2*/
+const solution = str => str.split('').reverse().join('');
+
+
+/* Best Practices Solution 3*/
+const solution = s => [...s].reverse().join('')
